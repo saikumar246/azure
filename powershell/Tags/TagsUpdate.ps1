f8708c6f87c8362844dc0c69bd981333e194c5e5
sai <52447619+saikumar246@users.noreply.github.com>
2022-09-20 00:21:05 +0530

    Update TagsUpdate.ps1

--- powershell/Tags/TagsUpdate.ps1
+++ powershell/Tags/TagsUpdate.ps1
@@ -1,17 +1,44 @@
 
+#=============================Parameter Section============================================================#
+
 Write-Host ("#######################################################################################################") -ForegroundColor DarkGray
 Write-Host ("############################## Adding tags to the VM and its associated items #########################") -ForegroundColor DarkGray
 Write-Host ("#######################################################################################################") -ForegroundColor DarkGray
 $InputFile= Read-Host -prompt "InputFile"
+$RG=Read-Host -prompt "ResourceGroupName"
 $VMList = Get-Content $InputFile
+Write-Host("---------------------------------------------------------------------------------------------------------") -ForegroundColor DarkGray
 Write-Host("List of Servers:") -ForegroundColor Magenta
-$VMList
-$tags= @{Location="myLocation";Env="test";Application="SaiWebsite";Owner="Sai Kumar <Sai@gmail.com>"}
+$VMList[1..$VMList.Count]
+#$tags= @{Location="myLocation";Env="prod";Application="SaiWebsite";Owner="Sai Kumar <Sai@gmail.com>";Support="CloudOps pod1";maintenance="23:00"}
+$tags=(Invoke-Expression $VMList[0])
 #Defining a Array of Vms 
 #$VMList=@("AzureSite-VM","MyDockerVM","Onrem-VM")
+#The 'merge' option allows adding tags with new names and updating the values of tags with existing names.
+#The 'replace' option replaces the entire set of existing tags with a new set.
 $action="Merge"
+
+Write-Host("---------------------------------------------------------------------------------------------------------") -ForegroundColor DarkGray
+Write-Host("####Check the Tags before adding to the resources ####") -ForegroundColor Magenta
+$tags
+Write-Host("---------------------------------------------------------------------------------------------------------") -ForegroundColor DarkGray
+
+$check=Read-Host -prompt("*********Continue? (yes/No)")
+if($check -eq "No"){ 
+Write-Host("Terminating the script and exit.....") -ForegroundColor Green
+Start-Sleep -Seconds 2
+exit(0)}
+
+Update-AzTag -ResourceId (Get-AzResourceGroup -name $RG).ResourceId -Tag $tags -Operation $action | Out-Null
+if($?){
+Write-Host ("### Tags {0}d on resource Group: '{1}' Successfully ### `n" -f $action, $RG) -ForegroundColor Green
+} else {
+Write-Host ("### Tags {0} on resource Group: '{1}' failed ### `n" -f $action, $RG) -ForegroundColor DarkRed
+}
+
 #=============================Loop Section=================================================================#
-foreach($VM in $VMList){
+for($j=1;$j -lt $VMList.Count;$j=$j+1){
+$VM=$VMList[$j]
 #Getting VM resource ID
 Write-Host("### {0}  ###" -f $VM) -ForegroundColor yellow
 $a=(get-azvm -name $VM).Id
@@ -63,7 +90,17 @@ Write-Host ("### Tags {0}d on resource: '{1}' Successfully ### `n" -f $action, (
 } else {
 Write-Host ("### Tags {0} on resource: '{1}' failed ### `n" -f $action, (Get-Azdisk -name $Disks.DataDisks.Name).Name) -ForegroundColor DarkRed
 }
+}
+}
+}
+Write-Host("---------------------------------------------------------------------------------------------------------") -ForegroundColor DarkGray
+if($?){
+Write-Host("Script executed Successfully, exiting the script....") -ForegroundColor Green
+Start-Sleep -Seconds 2
+}
+else{Write-Host("Scrit executed with errors, exiting the script") -ForegroundColor red}
 #-------------------------------------------------------#end of the foreach section---------------------------------
-}
-}
-}
+
+
+
+
