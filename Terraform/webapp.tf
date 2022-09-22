8d54d15ef4d8dcdfa6e8d4bfe104e6af39e3bb13
sai <52447619+saikumar246@users.noreply.github.com>
2022-09-22 21:00:06 +0530

    Update webapp.tf

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -6,7 +6,7 @@ terraform {
     storage_account_name = "__terraformstorageaccount__"
     container_name       = "terraform1"
     key                  = "terraform.tfstate"
-    access_key = "__storagekey__"
+    #access_key = "__storagekey__"
   }
 }
 
